4c1cc365a4c77ee6906e371af24b71705b2aa293
Harry Bigos <hrbigos@gmail.com>
2019-02-17 21:08:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // add solution here
+
+const musicians = [
+  'John Lennon',
+  'Paul McCartney',
+  'George Harrison',
+  'Ringo Starr'];
+  
+const instruments = [
+  'Guitar',
+  'Bass Guitar',
+  'Lead Guitar',
+  'Drums' ];
+  
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < 4; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+
+
+
+
+function johnLennonFacts(facts) {
+var facts2 = [];
+var i = 0;
+  while (i < facts.length) {
+    facts2.push(facts[i] + "!!!");
+    i++;
+  }
+  return facts2;
+}
+
+
+
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  var i = 0;
+  do {
+    array.push("I love the Beatles!");
+  }
+  while (array.length < 15);
+  i++;
+return array;
+}
